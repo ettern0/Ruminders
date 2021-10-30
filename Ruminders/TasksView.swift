fc2598db1247d9198fb3b31388b2da62378450a1
ettern0 <evserdyukov@gmail.com>
2021-10-30 20:48:00 +0200

    view Tasks

---
+++ Ruminders/TasksView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct TasksView: View {
+
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Tasks.timestamp, ascending: true)],
+        animation: .default)
+    //private var tasks: FetchedResults<Tasks>
+
+    var body: some View {
+        NavigationView {
+            List {
+                
+            }
+        }
+    }
+}
